4ed78b4343ad29e31ffd7a91ce22b91e5587e361
ElisaMavilia <elisamavilia1@gmail.com>
2024-06-10 17:05:39 +0200

    printed projects/added pagination

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,29 @@
 <template>
-  <h1 class="text-center">Ciao</h1>
-  <i class="fa fa-solid fa-home"></i>
+  <!-- {{ projects }} -->
+  <h1 class="text-center">Elenco dei progetti</h1>
+  <i class="fa fa-solid fa-suitcase ps-4"></i>
+  <ul>
+    <li v-for="project in projects" :key="project.id">{{ project.title }}
+      <img v-if="project.image" :src="project.image" :alt="project.title">
+    </li>
+  </ul>
+  <nav aria-label="Page navigation example">
+  <ul class="pagination">
+    <li class="page-item">
+      <a @click="getAllProjects()"class="page-link" href="#" aria-label="Previous">
+        <span aria-hidden="true">&laquo;</span>
+      </a>
+    </li>
+    <li class="page-item"><a class="page-link" href="{{ project.results.first_page }}">1</a></li>
+    <li class="page-item"><a @click="getAllProjects()" class="page-link" href="#">2</a></li>
+    <li class="page-item"><a @click="getAllProjects()" class="page-link" href="#">3</a></li>
+    <li class="page-item">
+      <a @click="getAllProjects()" class="page-link" href="#" aria-label="Next">
+        <span aria-hidden="true">&raquo;</span>
+      </a>
+    </li>
+  </ul>
+</nav>
 </template>
 
 <script>
@@ -10,13 +33,19 @@ import axios from 'axios';
     name: 'App',
     data() {
       return {
-        store
+        store,
+        projects: [],
+        nextPage: 1,
+        currentPage: 1,
       }
     },
     methods: {
       getAllProjects() {
-        axios.get(this.store.apiBaseUrl + '/projects').then((res) => {
+        axios.get(this.store.apiBaseUrl + '/projects', { params: { page: this.nextPage } }).then((res) => {
           console.log(res.data);
+          this.projects = res.data.results.data;
+          this.currentPage = res.data.results.current_page;
+          this.nextPage = this.currentPage +1;
         });
       },
   },
